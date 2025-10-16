72334896a1258d639326671bb1c62e849559f34a
Rick Steeves <github@corwyn.net>
2025-10-16 17:02:00 -0400

    Add files via upload

--- assets/frontend.js
+++ assets/frontend.js
@@ -27,6 +27,57 @@ jQuery(document).ready(function($) {
             scrollTop: $('#reservation-form').offset().top - 100
         }, 500);
     });
+       
+    // Field camping selection
+    $('#field-camping-btn').on('click', function() {
+        $('#field-camping-form').show();
+        $('#site-legend').hide();
+        $('#site-grid').hide();
+        $('#reservation-form').hide();
+        
+        $('html, body').animate({
+            scrollTop: $('#field-camping-form').offset().top - 100
+        }, 500);
+    });
+
+    // Field camping form submission
+    $('#ccc-wor-field-camping-form').on('submit', function(e) {
+        e.preventDefault();
+        
+        var guestCount = parseInt($('#field_guest_count').val());
+        var guestNames = $('#field_guest_names').val().trim().split('\n').filter(function(name) {
+            return name.trim() !== '';
+        });
+        
+        if (guestCount !== guestNames.length) {
+            alert('Number of people must match the number of names provided.');
+            return;
+        }
+        
+        // Submit via AJAX
+        $.ajax({
+            url: ccc_wor_ajax.ajax_url,
+            type: 'POST',
+            data: {
+                action: 'ccc_wor_frontend_action',
+                ccc_action: 'make_reservation',
+                nonce: ccc_wor_ajax.nonce,
+                site_id: 'field_camping',
+                guest_count: guestCount - 1, // Subtract 1 since guest_count is additional guests
+                guest_names: $('#field_guest_names').val()
+            },
+            success: function(response) {
+                if (response.success) {
+                    window.location.href = response.data.redirect_url;
+                } else {
+                    alert(response.data);
+                }
+            },
+            error: function() {
+                alert('An error occurred. Please try again.');
+            }
+        });
+    });
     
     // Form submission
     $('#ccc-wor-reservation-form').on('submit', function(e) {
@@ -85,3 +136,9 @@ function cancelSelection() {
     jQuery('#ccc-wor-reservation-form')[0].reset();
 }
 
+function cancelFieldSelection() {
+    jQuery('#field-camping-form').hide();
+    jQuery('#site-legend').show();
+    jQuery('#site-grid').show();
+    jQuery('#ccc-wor-field-camping-form')[0].reset();
+}
